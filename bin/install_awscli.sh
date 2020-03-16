b4fd70601faa7960c549c85644077bafc8769fa9
Alex Hope <alex.hope@shopify.com>
2020-03-16 10:44:54 -0400

    Move chmod's into compile and out of install script

--- bin/install_awscli.sh
+++ bin/install_awscli.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-INSTALL_DIR="/app/vendor/awscli"
-chmod +x /app/vendor/awscli-bundle/install
-/app/vendor/awscli-bundle/install -i $INSTALL_DIR
-chmod u+x $INSTALL_DIR/bin/aws
-
 export PATH=~/vendor/awscli/bin:$PATH
 
 mkdir -p ~/.aws
